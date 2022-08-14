ec3bb0b20310457f4a910a56d953d636e33e9cf4
m-costantini <marina.costant@gmail.com>
2022-08-14 20:03:31 +0200

    Finished retouching image format, implemented saving in loop

--- norm_balls.py
+++ norm_balls.py
@@ -1,39 +1,78 @@
 import numpy as np
-import plotly.graph_objects as go
 from plotly.subplots import make_subplots
+import plotly.graph_objects as go
 from math import sqrt, pi
 
-# L2 ball
-u, v = np.mgrid[0:2*np.pi:500j, 0:2*np.pi:500j]
-x2 = np.sin(u) * np.cos(v)
-y2 = np.sin(u) * np.sin(v)
-z2 = np.cos(u)
-fig = go.Figure(data=[go.Surface(x=x2, y=y2, z=z2, opacity=0.5, colorscale="Bluered", showscale=False)])
-fig.show()
+t = np.linspace(0,2*pi,36) # for parametric curve defining camera position
 
-# L1 ball by normalization
-rho = 1/(np.abs(x2) + np.abs(y2) + np.abs(z2))
-x1 = rho * np.sin(u) * np.cos(v)
-y1 = rho * np.sin(u) * np.sin(v)
-z1 = rho * np.cos(u)
-fig = go.Figure(data=[go.Surface(x=x1, y=y1, z=z1, opacity=0.5, colorscale="Bluered", showscale=False)])
-fig.show()
+for idx_t, t in enumerate(t):
 
+    print('Generating figure', t+1, '/', len(t), '...', end='')
+    sqrt_mag = 2.1
+    eye_x = sqrt_mag*np.sin(t)
+    eye_y = sqrt_mag*np.cos(t)
 
-# Linf ball by normalization
-M = np.concatenate((x2[:,:,np.newaxis], y2[:,:,np.newaxis], z2[:,:,np.newaxis]), axis=2)
-rho = 1/np.max(np.abs(M),axis=2)
-xinf = rho * np.sin(u) * np.cos(v)
-yinf = rho * np.sin(u) * np.sin(v)
-zinf = rho * np.cos(u)
-fig = go.Figure(data=[go.Surface(x=xinf, y=yinf, z=zinf, opacity=0.5, colorscale="Bluered", showscale=False)])
-fig.show()
+    pad = 0
+    subplot_specs = {'type': 'surface', 'l':pad, 'r':pad, 't':pad, 'b':pad}
+    titles = [r'$\large{\alpha=1}$', r'$\large{\alpha=2}$', r'$\large{\alpha=3}$', r'$\large{\alpha=4}$']
+    fig = make_subplots(rows=2, cols=2,
+        specs=[[subplot_specs, subplot_specs], [subplot_specs, subplot_specs]],
+        subplot_titles=titles,
+        horizontal_spacing = 0.0, vertical_spacing = 0.0)
 
+    # ------------------------------------------------------
+    #  MAKE BALLS
 
-# Lhalf ball by normalization
-rho = 1/( np.sqrt(np.abs(x2)) + np.sqrt(np.abs(y2)) + np.sqrt(np.abs(z2)) )**2
-xhalf = rho * np.sin(u) * np.cos(v)
-yhalf = rho * np.sin(u) * np.sin(v)
-zhalf = rho * np.cos(u)
-fig = go.Figure(data=[go.Surface(x=xhalf, y=yhalf, z=zhalf, opacity=0.5, colorscale="Bluered", showscale=False)])
-fig.show()
+    # L2 ball
+    u, v = np.mgrid[0:2*np.pi:500j, 0:2*np.pi:500j]
+    x2 = np.sin(u) * np.cos(v)
+    y2 = np.sin(u) * np.sin(v)
+    z2 = np.cos(u)
+    fig.add_trace(go.Surface(x=x2, y=y2, z=z2, opacity=0.5, colorscale="Bluered", showscale=False), row=2, col=1)
+
+    # L1 ball by normalization
+    rho = 1/(np.abs(x2) + np.abs(y2) + np.abs(z2))
+    x1 = rho * np.sin(u) * np.cos(v)
+    y1 = rho * np.sin(u) * np.sin(v)
+    z1 = rho * np.cos(u)
+    fig.add_trace(go.Surface(x=x1, y=y1, z=z1, opacity=0.5, colorscale="Bluered", showscale=False), row=1, col=2)
+
+    # Linf ball by normalization
+    M = np.concatenate((x2[:,:,np.newaxis], y2[:,:,np.newaxis], z2[:,:,np.newaxis]), axis=2)
+    rho = 1/np.max(np.abs(M),axis=2)
+    xinf = rho * np.sin(u) * np.cos(v)
+    yinf = rho * np.sin(u) * np.sin(v)
+    zinf = rho * np.cos(u)
+    fig.add_trace(go.Surface(x=xinf, y=yinf, z=zinf, opacity=0.5, colorscale="Bluered", showscale=False), row=2, col=2)
+
+    # Lhalf ball by normalization
+    rho = 1/(np.sqrt(np.abs(x2))+np.sqrt(np.abs(y2))+np.sqrt(np.abs(z2)))**2
+    xhalf = rho * np.sin(u) * np.cos(v)
+    yhalf = rho * np.sin(u) * np.sin(v)
+    zhalf = rho * np.cos(u)
+    fig.add_trace(go.Surface(x=xhalf, y=yhalf, z=zhalf, opacity=0.5, colorscale="Bluered", showscale=False), row=1, col=1)
+
+    # ------------------------------------------------------
+
+    axis_dict = dict(tickvals=[-1,0,1], showticklabels=False, showgrid=True, gridcolor="gray", showbackground=False)
+    scene_dict = dict(xaxis=axis_dict, yaxis=axis_dict, zaxis=axis_dict,
+        xaxis_title='', yaxis_title='', zaxis_title='', # no axis labels
+        camera=dict(eye=dict(x=eye_x, y=eye_y, z=1.2))) # set camera position (zoom+angle)
+
+    the_margin = go.layout.Margin(l=0, r=0, b=0, t=0) # gte rid of white margins
+    fig.update_layout(scene1=scene_dict, scene2=scene_dict, scene3=scene_dict, scene4=scene_dict,  width=1000, height=1000, margin=the_margin)
+
+    # make the titles closer to the plots
+    fig['layout']['annotations'][0]['y'] = 0.95
+    fig['layout']['annotations'][1]['y'] = 0.95
+    fig['layout']['annotations'][2]['y'] = 0.45
+    fig['layout']['annotations'][3]['y'] = 0.45
+    # change title fontsaizes
+    for annotation in fig['layout']['annotations']:
+        annotation['font'] = {'size':26}
+
+    print('saving figure...', end='')
+    save_name = 'norm_ball_figures/balls_fig_' + str(idx_t+1) + '.png'
+    fig.write_image(save_name)
+
+    print('done')
