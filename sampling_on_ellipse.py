be90619be677ff144dc476b8e82903e9e6966a04
m-costantini <marina.costant@gmail.com>
2022-08-23 21:40:25 +0200

    Added transformation to sample uniformly from ellipse, added polar bar plots

--- sampling_on_ellipse.py
+++ sampling_on_ellipse.py
@@ -14,11 +14,13 @@ August 2022
 marina.costant@gmail.com
 """
 
-from math import pi, sin
+from math import pi, sqrt
 import plotly.express as px
 from plotly.subplots import make_subplots
 import plotly.graph_objects as go
 import numpy as np
+from scipy import linalg
+
 
 # generate parametric rotated ellipse for plotting
 
@@ -32,45 +34,152 @@ x_ell = Rx*np.cos(t)*np.cos(theta) - Ry*np.sin(t)*np.sin(theta)
 y_ell = Rx*np.cos(t)*np.sin(theta) + Ry*np.sin(t)*np.cos(theta)
 
 # Circle
-R = 0.8
+R = 1
 x_circ = R*np.cos(t)
 y_circ = R*np.sin(t)
 
+# Ellipse from circle
+a = sqrt(Rx)
+b = sqrt(Ry)
+A = a**2*np.sin(theta)**2 + b**2*np.cos(theta)**2
+B = 2*(b**2-a**2)*np.sin(theta)*np.cos(theta)
+C = a**2 * np.cos(theta)**2 + b**2 * np.sin(theta)**2
+V = np.vstack((x_circ,y_circ))
+S = np.array([[A,B/2],[B/2,C]])
+W = S @ V
+# print(S)
+# S_inv = linalg.inv(S)
+# print(S_inv)
+S_inv_sqrt = linalg.inv(linalg.sqrtm(S))
+# print(S_inv_sqrt)
+x_ell_2 = W[0,:]
+y_ell_2 = W[1,:]
+
+# Figure
 titles = ['Circle', 'Ellipse 1', 'Ellipse 2']
-fig = make_subplots(rows=1, cols=3, subplot_titles=titles)
-fig.add_trace(go.Scatter(x=x_circ, y=y_circ), row=1, col=1)
-fig.add_trace(go.Scatter(x=x_ell, y=y_ell), row=1, col=2)
-fig.add_trace(go.Scatter(x=x_ell, y=y_ell), row=1, col=3)
+subplot_specs = {'type': 'barpolar'}
+fig = make_subplots(rows=1, cols=3, subplot_titles=titles, specs=[[subplot_specs, subplot_specs, subplot_specs]])
 
+# -----------------------------------------------------
+# Plotting the curves
+# -----------------------------------------------------
+# fig.add_trace(go.Scatter(x=0.8*x_circ, y=0.8*y_circ), row=1, col=1) # 0.8 for it to look better
+# fig.add_trace(go.Scatter(x=x_ell, y=y_ell), row=1, col=2)
+# fig.add_trace(go.Scatter(x=x_ell_2, y=y_ell_2), row=1, col=3)
 
-fig.update_yaxes(range=[-1,1], scaleanchor="x", scaleratio=1, constrain="domain")
-fig.update_xaxes(range=[-1,1])
+fig.update_yaxes(range=[-2,2], scaleanchor="x", scaleratio=1, constrain="domain")
+fig.update_xaxes(range=[-2,2])
 
-n_rounds = 3
-n_points = 100
-delta_radius = 0.03
-for r in range(n_rounds):
+# fig.update_yaxes(scaleanchor="x", scaleratio=1, constrain="domain")
 
-    Rx += delta_radius
-    Ry += delta_radius
-    R += delta_radius
-
-    t = np.random.uniform(low=0.0, high=2*pi, size=(n_points,))
-
-    x_ell = Rx*np.cos(t)*np.cos(theta) - Ry*np.sin(t)*np.sin(theta)
-    y_ell = Rx*np.cos(t)*np.sin(theta) + Ry*np.sin(t)*np.cos(theta)
-    x_circ = R*np.cos(t)
-    y_circ = R*np.sin(t)
-
-    fig.add_trace(go.Scatter(x=list(x_circ), y=list(y_circ),
-        mode='markers', marker=dict(size=3, color='blue')), row=1, col=1)
-    fig.add_trace(go.Scatter(x=list(x_ell), y=list(y_ell),
-        mode='markers', marker=dict(size=3, color='red')), row=1, col=2)
-    fig.update_layout(showlegend=False)
+# -----------------------------------------------------
+# Plotting points
+# -----------------------------------------------------
+# n_rounds = 20
+# n_points = 50
+# delta_radius = 0.03
+# for r in range(n_rounds):
+#
+#     # Rx += delta_radius
+#     # Ry += delta_radius
+#     # R += delta_radius
+#
+#     Rx *= 1.02
+#     Ry *= 1.02
+#     R *= 1.02
+#
+#     t = np.random.uniform(low=0.0, high=2*pi, size=(n_points,))
+#
+#     x_ell = Rx*np.cos(t)*np.cos(theta) - Ry*np.sin(t)*np.sin(theta)
+#     y_ell = Rx*np.cos(t)*np.sin(theta) + Ry*np.sin(t)*np.cos(theta)
+#     x_circ = R*np.cos(t)
+#     y_circ = R*np.sin(t)
+#     V = np.vstack((x_circ,y_circ))
+#     W = S_inv_sqrt @ V
+#     x_ell_2 = W[0,:]
+#     y_ell_2 = W[1,:]
+#
+#     fig.add_trace(go.Scatter(x=list(x_circ), y=list(y_circ),
+#         mode='markers', marker=dict(size=3, color='blue')), row=1, col=1)
+#     fig.add_trace(go.Scatter(x=list(x_ell), y=list(y_ell),
+#         mode='markers', marker=dict(size=3, color='red')), row=1, col=2)
+#     fig.add_trace(go.Scatter(x=list(x_ell_2), y=list(y_ell_2),
+#         mode='markers', marker=dict(size=3, color='green')), row=1, col=3)
+#
+#     fig.update_layout(showlegend=False)
 
     # # save this frame
     # save_name = 'sampling_on_ellipse_figures/ellipse_fig_' + str(r+1) + '.png'
     # fig.write_image(save_name)
     # print('Saved figure', r+1, '/', n_rounds)
+
+
+# -----------------------------------------------------
+# Polar plot
+# -----------------------------------------------------
+n_points = 10000
+
+t = np.random.uniform(low=0.0, high=2*pi, size=(n_points,))
+
+# x_ell = Rx*np.cos(t)*np.cos(theta) - Ry*np.sin(t)*np.sin(theta)
+# y_ell = Rx*np.cos(t)*np.sin(theta) + Ry*np.sin(t)*np.cos(theta)
+x_circ = R*np.cos(t)
+y_circ = R*np.sin(t)
+V = np.vstack((x_circ,y_circ))
+W = S_inv_sqrt @ V
+x_ell_2 = W[0,:]
+y_ell_2 = W[1,:]
+
+t_prime = np.arctan2(y_ell_2, x_ell_2)
+
+n_bins = 36
+bins_pi = np.linspace(0,2*pi,n_bins+1)
+bins = bins_pi*360/(2*pi)
+d_bin = bins[1]
+
+counts = np.histogram(t, bins=bins_pi)[0]
+norm_counts_t = counts/max(counts)
+counts = np.histogram(t_prime, bins=bins_pi)[0]
+norm_counts_t_prime = counts/max(counts)
+
+# fig = go.Figure(go.Barpolar(
+#     r=[3.5, 1.5, 2.5, 4.5, 4.5, 4, 3],
+#     theta=[65, 15, 210, 110, 312.5, 180, 270],
+#     width=[20,15,10,20,15,30,15,],
+#     marker_color=["#E4FF87", '#709BFF', '#709BFF', '#FFAA70', '#FFAA70', '#FFDF70', '#B6FFB4'],
+#     marker_line_color="black",
+#     marker_line_width=2,
+#     opacity=0.8
+# ))
+#
+# fig.update_layout(
+#     template=None,
+#     polar = dict(
+#         radialaxis = dict(range=[0, 5], showticklabels=False, ticks=''),
+#         angularaxis = dict(showticklabels=False, ticks='')
+#     )
+# )
+
+
+fig.add_trace(go.Barpolar(
+    r=norm_counts_t,
+    theta=bins+(d_bin/2),
+    width=d_bin*np.ones((len(norm_counts_t),)),
+    marker_line_color="black",
+    marker_line_width=2,
+    opacity=0.8
+    ), row=1, col=1)
+
+fig.add_trace(go.Barpolar(
+    r=norm_counts_t_prime,
+    theta=bins+(d_bin/2),
+    width=d_bin*np.ones((len(norm_counts_t_prime),)),
+    marker_line_color="black",
+    marker_line_width=2,
+    opacity=0.8
+    ), row=1, col=2)
+
+
+fig.update_layout(showlegend=False)
 
 fig.show()
